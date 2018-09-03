adb525156b8e3b452a22fe304405909cc23fe61b
Viacheslav Gordeiko <gordeikovs@gmail.com>
2018-09-03 18:02:16 +0300

    Add files via upload

---
+++ telegram.php
@@ -0,0 +1,31 @@
+<?php
+$token = "***";
+$chat_id = "***";
+
+$values = $hook->getValues();
+
+$formName = $modx->getOption('formName', $formit->config, 'form-'.$modx->resource->get('id'));
+
+// Get user IP
+$ip = $modx->getOption('REMOTE_ADDR', $_SERVER, '');
+
+#Data from form
+$name = $values['name'];
+$phone = $values['phone'];
+
+// Create array
+$arr = array(
+    "Name" => $name,
+    "Phone" => $phone,
+    "Form name" => $formName,
+    "IP" => $ip);
+
+// Create message
+foreach($arr as $key => $value) {
+    $txt .= "<b>".$key."</b>: ".$value."%0A";
+}
+
+// Send message
+$fp=fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+return true;
